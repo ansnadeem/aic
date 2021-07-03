ad5c17aedd19eb21ddf974c2fb64895555375291
Ans Nadeem <ans_nadeem@outlook.com>
2021-07-03 14:59:01 -0500

    Create constants.py

---
+++ app/utils/constants.py
@@ -0,0 +1,29 @@
+GITHUB_ACCESS_TOKEN_LINK = 'https://api.github.com/app/installations/{0}/access_tokens'
+LABEL_LINK_SUFFIX = '/labels'
+RESULT_MASK_ORDER = ["bug", "enhancement", "question"]
+GIT_APP_ENV_NAME = 'GITHUB_APP_ID'
+PEM_KEY_FILE_ENV_NAME = 'PRIVATE_KEY_PEM_FILE'
+TRAINED_MODEL_PATH = 'model'
+TEMPLATES_FOLDER = 'templates'
+ROBERTA = 'roberta'
+
+DOT_ENV_PATH = '.env'
+
+GITHUB_ACTION = 'action'
+OPEN_STATUS = 'opened'
+
+TOKEN = 'token'
+ISSUE = 'issue'
+TITLE = 'title'
+BODY = 'body'
+INSTALLATION = 'installation'
+ID = 'id'
+URL = 'url'
+
+RESULT_LABELS = 'labels'
+RESULT_THRESHOLD = 'threshold'
+
+RESPONSE_RESULT = 'result'
+RESPONSE_STATUS = 'status'
+
+HTTP_OK = 200
